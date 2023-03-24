333b8d22ef19088b62a18b07c2bad85ddacbfbbb
James Boyle <james.patrick.boyle92@gmail.com>
2023-03-24 11:07:45 +0100

    Initial commit

---
+++ src/App.tsx
@@ -0,0 +1,13 @@
+import './scss/app.scss'
+import About from './sections/about/About'
+
+function App() {
+
+  return (
+    <main>
+      <About />
+    </main>
+  )
+}
+
+export default App
---
+++ src/sections/about/About.tsx
@@ -0,0 +1,20 @@
+export default function About() {
+
+    return (
+        <section id="about">
+            <div>
+                
+            </div>
+            <div>
+                <h1>James Boyle</h1>
+                <h2>Web Developer</h2>
+                <p>Welcome to my web developer portfolio! I'm a passionate developer with experience in designing and developing web applications using modern technologies. With a focus on creating user-friendly and responsive web experiences, I strive to build dynamic and engaging websites that not only meet but exceed my clients' expectations.</p>
+            </div>
+            <div>
+                <button>icon</button>
+                <button>icon</button>
+                <button>icon</button>
+            </div>
+        </section> 
+    )
+} 
