edc0e98173937ee601bc291b6648862510308abb
James Boyle <james.patrick.boyle92@gmail.com>
2023-03-28 22:19:34 +0200

    Add main layout component

--- src/App.tsx
+++ src/App.tsx
@@ -1,23 +1,30 @@
+import MainLayout from './components/layouts/MainLayout'
 import SocialLinks from './components/SocialLinks'
 import './scss/app.scss'
 import About from './sections/about/About'
 import Experience from './sections/experience/Experience'
+import Navigation from './sections/navigation/Navigation'
 import Projects from './sections/projects/Projects'
 import TechStack from './sections/techStack/TechStack'
 import WhatCanIOffer from './sections/whatCanIOffer/WhatCanIOffer'
 
-function App() {
+export default function App() {
 
   return (
-    <main>
-      <About />
-      <Experience />
-      <TechStack />
-      <WhatCanIOffer />
-      <Projects />
-      <SocialLinks />
-    </main>
+    <>
+      <Navigation />
+
+      <MainLayout>
+
+        <About />
+        <Experience />
+        <TechStack />
+        <WhatCanIOffer />
+        <Projects />
+        <SocialLinks />
+
+      </MainLayout>
+    </>
   )
 }
 
-export default App
