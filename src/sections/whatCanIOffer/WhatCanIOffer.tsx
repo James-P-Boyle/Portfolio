ae723a0cbff7947c51e982d2fdf2008c5ee87281
James Boyle <james.patrick.boyle92@gmail.com>
2023-03-29 22:05:13 +0200

    update offer content

--- src/sections/whatCanIOffer/WhatCanIOffer.tsx
+++ src/sections/whatCanIOffer/WhatCanIOffer.tsx
@@ -9,20 +9,36 @@ const WhatCanIOffer = forwardRef<HTMLDivElement>((props, ref) => {
             <h2>What Can I Offer You</h2>
 
             <div>
-                <span><strong>SEO</strong> and <strong>Performance Reviews</strong></span>
-                <span><strong>Clean Architecture</strong> and <strong>Best Practices</strong></span>
-                <span><strong>Can Do Attitude</strong> and <strong>Willingness to Learn</strong></span>
-                <span><strong>Ability to collaborate</strong> effectively with <strong>cross-functional teams</strong></span>
-                <span>Ability to maintain a <strong>positive and upbeat attitude</strong> while still delivering <strong>high-quality work</strong> consistently and reliably</span>
-                <span>Ability to <strong>work well under pressure</strong> and meet <strong>tight deadlines</strong></span>
-                <span><strong>Creative thinking</strong> and ability to think outside the box</span>
-                <span><strong>Strong work ethic</strong> and genuine <strong>passion for learning</strong></span>
-                <span><strong>Fun yet reliable</strong> teammate</span>
-                <span><strong>Leadership skills</strong> and ability to take charge when needed</span>
-                <span><strong>Problem-solving skills</strong> and ability to find <strong>efficient solutions</strong> to complex issues</span>
-                <span><strong>Attention to detail</strong> and commitment to delivering <strong>bug-free</strong> code</span>
-                <span><strong>Effective communication skills</strong> and ability to articulate ideas clearly and concisely</span>
+                <p>
+                    I have a <strong>passion for learning</strong> and a <strong>can-do attitude</strong>, 
+                    coupled with <strong>strong work ethic</strong> and <strong>attention to detail </strong> 
+                    to deliver <strong>high-quality work</strong> consistently and reliably. 
+                    I have a <strong>problem-solving mindset</strong> and the ability to think creatively 
+                    and find <strong>efficient solutions</strong> to complex issues.
+                </p>
+                <p>
+                    I am committed to following <strong>clean architecture</strong> and 
+                    <strong> best practices</strong>, ensuring that all work is optimized for <strong>SEO </strong> 
+                    and <strong>performance</strong>. I possess <strong>leadership skills </strong> 
+                    and am able to take charge when needed, while maintaining a <strong>positive and upbeat attitude </strong> 
+                    that fosters collaboration and teamwork.
+
+                </p>
+                <p>
+                    I have the <strong>ability to communicate ideas clearly and concisely</strong>,
+                    and to work effectively with <strong>cross-functional teams</strong>. I am able to <strong>maintain 
+                    composure under pressure</strong> and meet <strong>tight deadlines</strong>. 
+                    I believe in <strong>thinking outside the box</strong> and bringing <strong>fun and creativity </strong> 
+                    to the table, while delivering <strong>bug-free code</strong>.
+                </p>
+                <p>
+                    If you're looking for a <strong>dedicated and reliable teammate</strong>, 
+                    feel free to get in contact with me any time!
+                </p>
             </div>
+            
+            
+            
 
         </section>
     )
